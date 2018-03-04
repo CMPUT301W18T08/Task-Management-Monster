8513afb17862d9d4974d4a56dd4bbc817e41c89e
Xixuan Song <songxixuan@MacBook-Pro.local>
2018-03-04 16:28:58 -0700

    user object update

--- app/src/main/java/com/example/yanghanwen/taskmanagementmonster/User.java
+++ app/src/main/java/com/example/yanghanwen/taskmanagementmonster/User.java
@@ -1,5 +1,7 @@
 package com.example.yanghanwen.taskmanagementmonster;
 
+import java.util.ArrayList;
+
 /**
 
  Created by yangwenhan on 2018/2/21. */
@@ -8,6 +10,7 @@ public class User {
     private String userName;
     private String email;
     private String phoneNum;
+    private ArrayList<Task>tasks;
 
     public User(String name, String email, String phoneNum) {
         this.userName = name;
