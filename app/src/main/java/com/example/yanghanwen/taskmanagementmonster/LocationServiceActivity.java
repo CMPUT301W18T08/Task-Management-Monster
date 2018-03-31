0ca154707d760b8fd1682a1237919625366f5b16
yanghanwen <yanghanwen@yanghanwendeMacBook-Pro.local>
2018-03-30 20:19:43 -0600

    new UI and geolocation

---
+++ app/src/main/java/com/example/yanghanwen/taskmanagementmonster/LocationServiceActivity.java
@@ -0,0 +1,69 @@
+package com.example.yanghanwen.taskmanagementmonster;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class LocationServiceActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_service);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng toronto = new LatLng(43.6532, -79.3832);
+        mMap.addMarker(new MarkerOptions().position(toronto).title("Marker in Toronto"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(toronto));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+        //https://stackoverflow.com/questions/33666071/android-marshmallow-request-permission
+        //2018-03-30
+        else {
+            ActivityCompat.requestPermissions(LocationServiceActivity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+
+            case 1:
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+                    mMap.setMyLocationEnabled(true);
+                }
+                else {
+                    Toast.makeText(LocationServiceActivity.this, "Permission denied to access your location", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            }
+        }
+    }
--- app/src/main/java/com/example/yanghanwen/taskmanagementmonster/MainActivity.java
+++ app/src/main/java/com/example/yanghanwen/taskmanagementmonster/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.yanghanwen.taskmanagementmonster;
 
 import android.content.Intent;
-import android.app.ActionBar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBar;
 
 /**
  * The activity control the main view of the app
@@ -15,14 +22,17 @@ public class MainActivity extends AppCompatActivity {
 
     public static MainModel mainModel;  // model of this activity
 
-    private Button newTaskButton;       // button to create new task
-    private Button searchButton;        // button to search a task
+    private FloatingActionButton newTaskButton;       // button to create new task
+    private ImageButton searchButton;
+    private ImageButton search_around;// button to search a task
     private Button providerButton;      // button to see my task as provider
     private Button requesterButton;     // button to see my task as requester
     private Button profileButton;       // button to see my profile
+    private DrawerLayout mdrawerlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -33,18 +43,33 @@ public class MainActivity extends AppCompatActivity {
         // create the model by given the username
         mainModel = new MainModel(username);
 
-        newTaskButton = (Button) findViewById(R.id.newTaskButton);
-        searchButton = (Button) findViewById(R.id.mainSearch);
+        newTaskButton = (FloatingActionButton) findViewById(R.id.newTaskButton);
+        //searchButton = (ImageButton) findViewById(R.id.mainSearch);
         providerButton = (Button) findViewById(R.id.providerButton);
         requesterButton = (Button) findViewById(R.id.requesterButton);
         profileButton = (Button) findViewById(R.id.profileButton);
 
+        mdrawerlayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        search_around = (ImageButton) findViewById(R.id.search_location);
 
-        // button to create new task
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(null);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+
+        ActionBar actionbar = getSupportActionBar();
+
+        if(actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setHomeAsUpIndicator(R.drawable.icon_list_24dp);
+        }
+
+
         newTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent(MainActivity.this,
                         NewTaskActivity.class);
 
@@ -52,56 +77,80 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // button to search a task
-        searchButton.setOnClickListener(new View.OnClickListener() {
+
+        search_around.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LocationServiceActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        //TODO switch...case instead of click Listener
+
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.profileButton:
+                        Intent intent = new Intent(MainActivity.this,
+                                EditprofileActivity.class);
+
+                        startActivity(intent);
+                        break;
+
+                    case R.id.providerButton:
+                        Intent intent1 = new Intent(MainActivity.this, MyTaskActivity.class);
+
+                        // set the type as provider
+                        intent1.putExtra("type", "pro");
+
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.requesterButton:
+                        Intent intent2 = new Intent(MainActivity.this, MyTaskActivity.class);
+
+                        // set the type as requester
+                        intent2.putExtra("type", "req");
+
+                        startActivity(intent2);
+                        break;
+
+                    default:
+                }
+                return true;
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+
+            case android.R.id.home:
+                mdrawerlayout.openDrawer(GravityCompat.START);
+                break;
+
+            case R.id.search_item:
 
                 Intent intent = new Intent(MainActivity.this, SearchActivity.class);
 
                 startActivity(intent);
-            }
-        });
+                break;
 
-        // button to see my task as provider
-        providerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(MainActivity.this, MyTaskActivity.class);
-
-                // set the type as provider
-                intent.putExtra("type", "pro");
-
-                startActivity(intent);
-            }
-        });
-
-        // button to see my task as requester
-        requesterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(MainActivity.this, MyTaskActivity.class);
-
-                // set the type as requester
-                intent.putExtra("type", "req");
-
-                startActivity(intent);
-            }
-        });
-
-        // button to see my profile
-        profileButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(MainActivity.this,
-                        EditprofileActivity.class);
-
-                startActivity(intent);
-            }
-        });
-
+            default:
+        }
+        return true;
+        }
     }
-}
+
